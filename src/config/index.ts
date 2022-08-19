c6f105ff650a4a72f44714fd67db8054ad74ee2b
Jae Min Ko <jvc123456@khu.ac.kr>
2022-08-19 22:33:29 +0900

    [17] Chore : server port와 p2p port 분리 및 api prefix 추가

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config';
-import * as process from 'process';
+import process from 'process';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 export default{
-    PORT : <number>Number(process.env.PORT),
-    HASH_ALGORITHM : <string>process.env.HASH_ALGORITHM
+    SERVER_PORT : <number> Number(process.env.SERVER_PORT),
+    P2P_PORT : <number> Number(process.env.P2P_PORT),
+    HASH_ALGORITHM : <string>process.env.HASH_ALGORITHM,
+    API : {
+        prefix : <string>'/api'
+    }
 }
