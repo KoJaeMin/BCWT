018dabd38f9bca5cc53f13562901bdcf7b869cdd
Jae Min Ko <jvc123456@khu.ac.kr>
2022-08-21 23:57:12 +0900

    [27] Feat : 서버 mining 기능 추가

--- src/api/routes/p2p.ts
+++ src/api/routes/p2p.ts
@@ -3,15 +3,21 @@ import p2p from '../../core/p2p';
 
 const route : Router = Router();
 
-export default (app : Router , ws : p2p) =>{
+export default (app : Router , p2p : p2p) =>{
     app.use('/p2p', route);
 
     route.get('/', (req : Request, res : Response)=>{
-        /// Nothing
+        /// nothing
     });
 
     route.post('/', (req : Request, res : Response)=>{
         /// p2p 서버 연결
-        res.status(200).json()
+        const { peer } = req.body;
+        p2p.connectToPeer(peer)
+    })
+    route.post('/blockchain',(req : Request, res : Response)=>{
+        const { data } = req.body;
+        const newBlock = p2p.addBlock(data);
+        res.json(newBlock)
     })
 }
