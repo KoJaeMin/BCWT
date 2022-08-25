b3bed6c638165cb19b84042fd3ad6225cedf2574
Jae Min Ko <jvc123456@khu.ac.kr>
2022-08-25 14:36:01 +0900

    [32] Refactor : BlockShape type file로 분리

---
+++ @types/BlockShape.d.ts
@@ -0,0 +1,10 @@
+declare interface BlockShape{
+    hash : string,
+    preHash : string,
+    data : string,
+    timestamp: number,
+    height : number,
+    nonce : number,
+    difficulty : number,
+    
+}
